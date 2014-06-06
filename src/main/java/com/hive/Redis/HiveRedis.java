c0c3d9575bde1c459d705bb6d19f61d9b5c9e421
Glacier <OurHom.759@gmail.com>
2014-06-06 20:13:21 +0800

    更新Redis 修正getLength()方法抛出异常

--- src/main/java/com/hive/Redis/HiveRedis.java
+++ src/main/java/com/hive/Redis/HiveRedis.java
@@ -1,94 +1,56 @@
-
-import java.util.HashSet;
+package com.hive.Redis;
 
 import redis.clients.jedis.Jedis;
 
-public class HiveRedis implements Runnable {
-	static private String SecUsername = null;
+public class HiveRedis {
+	
 	private String Key;
 	private Jedis Redis;
-	private HashSet<String> Keys = new HashSet<String>();
-	private long ChangeCount, LastLength, NowLength = 0;
 
-/*	public static void main(String[] args) {
-	//	HiveRedis obj = new HiveRedis();
-		//obj.ConnectRedis("222.24.63.100");
-		//obj.setKey("URL");
-		//System.out.println(obj.getLength());
-		//obj.clearRedis();
-		//System.out.println(obj.getLength());
+	public static void main(String[] args) {
+	HiveRedis obj = new HiveRedis();
+	System.out.println(getSecount("URL"));
+		obj.ConnectRedis("222.24.63.100", 9004);
+		obj.setKey("URL");
+		obj.pushValue("www.baidu.com");
+		obj.pushValue("www.google.com");
+		System.out.println(obj.getLength());
+		System.out.println(obj.popValue());
+		System.out.println(obj.popValue());
+		System.out.println(obj.getLength());
+		obj.Redis.flushAll();
 	}
-*/
-	public long Sechange() {
-		if ( SecUsername == null ) {
-			return 0;
-		}
-		else {
-			return this.ChangeCount;
-		}
+	static public int getSecount( String username ) {
+		return new Jedis("222.24.63.100", 9004).llen(username).intValue();
 	}
-	
 	public void ConnectRedis(String host, int port) {
 		Redis = new Jedis(host, port);
-		new Thread(this).start();
 	}
 	public void ConnectRedis( String host ) {
 		Redis = new Jedis(host, 6379);
-		new Thread(this).start();
 	}
 	public void ConnectRedis() {
 		Redis = new Jedis("127.0.0.1", 6379);
-		new Thread(this).start();
 	}
 	public void setKey( String Key ) {
 		this.Key = Key;
-		Keys.add(Key);
-		SecUsername = "user";
-	}
-	public long getKeySize() {
-		return Keys.size();
 	}
 	public String getKey() {
 		return Key;
 	}
-	public String getValue() {
-		return Redis.lindex(Key, Redis.llen(Key)-1);
+	public void pushValue( String Value ) {
+		Redis.lpush(Key, Value);
 	}
 	public String popValue() {
-		return Redis.rpop(Key);
+		return Redis.rpoplpush(Key, "Temp");
 	}
-	public long pushValue( String Value ) {
-		Redis.lpush(Key, Value);
-		return Redis.llen(Key);
-	}
-	
-	public long getLength( String Key ) { 	//返回URL的个数
-		return Redis.llen(Key);
-	}
-	public long getLength() {
-		return Redis.llen(Key);
-	}
-	public String indexValue( long index ) {
-		return Redis.lindex(Key, Redis.llen(Key)-1-index);
+	public int getLength() {
+		return Redis.llen(Key).intValue();
 	}
 	public void delKey() {
 		Redis.del(Key);
 	}
-	public String clearRedis() {
-		return Redis.flushDB();
-	}
-
-	public void run() {
-		try {
-			while ( true ) {
-				LastLength = NowLength;
-				Thread.sleep(1000);
-				NowLength = this.getLength();
-				ChangeCount = NowLength-LastLength;
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
+	public void clearRedis() {
+		Redis.flushAll();
 	}
 }
